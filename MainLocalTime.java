fe55cc5aa5bce9dcaafa7600c2ad793d71393f10
Eishitha <102133159+Eishitha@users.noreply.github.com>
2022-03-25 20:56:54 +0530

    Create MainLocalTime.java

---
+++ MainLocalTime.java
@@ -0,0 +1,18 @@
+# 24-3-2022-Java-Programs
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+public class MainLocalTime {
+    public static void main(String[] args) {
+        LocalTime o1=LocalTime.now();
+        System.out.println(o1);
+
+        Instant i = Instant.now();
+        System.out.println(i);
+
+        for(String s :ZoneId.getAvailableZoneIds()){
+            System.out.println(s);
+        }
+    }
+}
